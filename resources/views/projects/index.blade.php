0d624356b7e3b28a57a5959f2b2d73a7697e9925
Xavir <xaviranik.me@gmail.com>
2019-06-09 21:21:27 +0600

    Added: TailwindCSS, Removed: Bootstrap. Redesigning front end

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,15 +1,17 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container">
-        <h1 class="display-4">All Projects</h1>
-        <ul>
-            @forelse($projects as $project)
-                <li>
-                    <a href="{{ $project->path() }}">{{ $project->title }}</a>
-                </li>
-            @empty
-                <h1 class="display-4">Nothing here!</h1>
-            @endforelse
-        </ul>
+    <div class="flex items-center mb-3">
+        <a href="{{ route('projects.create') }}">New Project</a>
+    </div>
+
+    <div class="flex">
+        @forelse($projects as $project)
+            <div class="bg-white rounded shadow p-5 mr-4 w-1/3" style="height: 200px">
+                <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
+                <div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+            </div>
+        @empty
+            <h4>No Projects yet!</h4>
+        @endforelse
     </div>
 @endsection
