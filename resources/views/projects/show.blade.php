dcc9e6e42e615bc44e1fe2dc0db094c49d20749d
Xavir <xaviranik.me@gmail.com>
2019-06-13 14:37:48 -0700

    Added: Activity card UI

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -56,6 +56,7 @@
 
             <div class="lg:w-1/4 px-3">
                 @include('projects.card')
+                @include('projects.activity.card')
             </div>
         </div>
     </main>
